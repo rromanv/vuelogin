acc307b2b41babb7587524eab6233d80f06a16be
Rodolfo Román <rromanv@users.noreply.github.com>
2021-11-02 23:20:13 -0500

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,52 @@
+import { createRouter, createWebHistory } from "vue-router";
+import useAuth from "./composable/useAuth";
+
+import Index from "./pages/index.vue";
+import About from "./pages/about.vue";
+import Login from "./pages/login.vue";
+import Secret from "./pages/secret.vue";
+import NotFound from "./pages/404.vue";
+
+const { isAuthenticated } = useAuth();
+
+const routes = [
+  {
+    path: "/",
+    name: "Index",
+    component: Index,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/secret",
+    name: "Secret",
+    component: Secret,
+    beforeEnter: (to, from, next) => {
+      console.log(isAuthenticated);
+      if (!isAuthenticated.value) {
+        next("/login");
+      }
+      next();
+    },
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: NotFound,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
