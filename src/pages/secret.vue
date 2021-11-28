47d274deca64ceea856179994ddf37f866fea853
Rodolfo Roman <rromanv@gmail.com>
2021-11-27 21:37:45 -0600

    Chat layout done

--- src/pages/secret.vue
+++ src/pages/secret.vue
@@ -1,14 +1,37 @@
 <template>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt autem
-    reprehenderit iusto qui, dolor explicabo, quae beatae officia eveniet
-    itaque, quia magnam sunt quaerat? Vero necessitatibus impedit maiores nobis
-    aperiam. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi
-    inventore magni temporibus asperiores sunt nihil maiores excepturi, velit
-    neque et non autem unde optio quos aspernatur blanditiis in eligendi
-    repellat? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-    fuga aspernatur numquam blanditiis at consectetur magnam, voluptatem
-    sapiente voluptate adipisci dolore nesciunt dignissimos! Vel omnis, nulla
-    corporis deserunt consequuntur ratione?
-  </p>
+  <h1 class="text-6xl font-thin tracking-tighter text-center mt-8">
+    Cool Chat
+  </h1>
+  <div
+    class="
+      min-h-[500px]
+      w-full
+      mt-8
+      rounded-lg
+      shadow-2xl
+      flex flex-col
+      justify-between
+    "
+  >
+    <ul class="p-4 space-y-4">
+      <li v-for="n in 1">
+        <div class="bg-gray-200 px-4 py-2 rounded-lg flex justify-between">
+          <span>Message</span><span>by Rodolfo</span>
+        </div>
+      </li>
+    </ul>
+    <div>
+      <input
+        type="text"
+        class="
+          w-full
+          p-4
+          rounded-lg
+          shadow-2xl
+          focus:outline-none focus:bg-yellow-100
+        "
+        placeholder="Type a message..."
+      />
+    </div>
+  </div>
 </template>
